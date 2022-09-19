b544070e1f1f9ea98eb05c534bfc1ac7fa802159
Mauricio Pantoja <mcpantoja@outlook.com>
2022-09-18 21:47:04 -0300

    first commit

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -8,11 +8,15 @@ namespace DotnetAula1.Models
     public class Pessoa
     {
         public string Nome { get; set; }
+
         public int Idade { get; set; }
+
+        public string Endereco { get; set;}
 
         public void Apresentar()
         {
-            Console.WriteLine($"Olá! meu nome {Nome} e tenho {Idade} anos.")
+           // Console.WriteLine($"Olá! meu nome é" + $"{Nome} tenho {Idade} anos e moro em {Endereco}.");
+              Console.WriteLine($"Olá! meu nome é " + $"{Nome} tenho {Idade}\nanos e moro em {Endereco}.");
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DotnetAula1.Models;
+
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "Mauricio";
+pessoa1.Idade = 42;
+pessoa1.Endereco = "Belém-Pará-Brasil";
+pessoa1.Apresentar();
+
